4f472384b5ce19d4057671d3080b1700a9be9e76
NayanPython <choudharyji527@gmail.com>
2026-03-31 11:27:34 +0530

    file is the operational brain of the request and response cycle, when clicked submit, sends email and flashed message

--- job_application/views.py
+++ job_application/views.py
@@ -27,8 +27,10 @@ def index(request):
 
     return render(request,"index.html")
 
+
 def about(request):
     return render(request, "about.html")
+
 
 def contacts(request):
     return render(request, "contacts.html")

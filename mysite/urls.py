f77b64374919bdf760fc781e08bc2ab7b5369ba9
NayanPython <choudharyji527@gmail.com>
2026-03-31 11:17:16 +0530

    Generated by django, The global routing map

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('job_application.urls'))
+    path('', include('job_application.urls'))  # map to locate urls
 ]

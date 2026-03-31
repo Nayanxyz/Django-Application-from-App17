86413a6d570ce05a7208539bc336943b564864f7
NayanPython <choudharyji527@gmail.com>
2026-03-31 11:16:16 +0530

    Generated by django, The master control panel

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'nayan7857@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'passwords'
